cfb0b3b3e65232fa7b36d2228407a836461e0881
esse-est <ijonkventar@gmail.com>
2025-10-24 12:44:14 -0500

    Refactor memory handling and remove unused code

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
+letter_lookup = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","r","u","v","w","x","y","z"]
 
 
+data="MAMbN SADxxHello_world!N PRIQEMxx".replace(" ","").split("N")
 
-mem={}
+MEM = {
+}
 
-code="SxahelN SxbloN Sxc_woN Sxdrld   PxaN PxbN PxcN Pxd".replace(" ","")
-
-def set_mem(addr,value):
-    mem[addr]=value
-
-
-
-#opc = {
-#    "Q":query_mem,
-#    "S":set_mem,
-#    "P":print
-#}
-
-
-for line in code.split("N"):
-    for c in range(0,len(line)-1):
-        if line[c] == "Q":
-            line=line.replace(line[c:c+3],mem[line[c+1:c+3]])
-        elif line[c] == "S":
-            mem[line[c+1:c+3]]=line[c+3:c+6]
-        elif line[c] == "P":
-            print(mem[line[c+1:c+3]],end="")
+MEM_ADDR_MAX = 0
+for line in data:
+    #memory opc
+    if line[:3] == "MAM":
+        #mam of 2 is 676 possible addresses, 3 in 17576
+        MEM_ADDR_MAX = letter_lookup.index(line[3])+1
+    elif line[:3] == "SAD":
+        MEM[line[3:(3+MEM_ADDR_MAX)]]=line[3+MEM_ADDR_MAX:]
+    
+    #qem swap
+    for qem in range(0,line.count("QEM")):
+        idval=line.find("QEM")
+        line=f"{line[:idval]}{MEM[line[1+idval+MEM_ADDR_MAX:]]}"
+    
+    #non mathamatical functions
+    if line[:3] == "PRI":
+        print(line[3:])
+    
